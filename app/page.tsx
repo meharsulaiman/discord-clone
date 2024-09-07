e60cda0ede9c4204db93427147f338c7a513bd8a
Sulaiman Shabbir <sulaimanshabbir31@gmail.com>
2024-09-07 21:35:51 +0500

    add fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
-  return <p className='text-3xl font-bold'>Hello World</p>;
+  return (
+    <div>
+      <Button>Click me</Button>
+      <h1>Discord clone</h1>
+    </div>
+  );
 }
