ad4867fa5bd9e0cdbea88ca0793e74d905cafc2d
Sulaiman Shabbir <sulaimanshabbir31@gmail.com>
2024-09-16 01:44:38 +0500

    fix chat messages component for fetching chat

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,5 +1,6 @@
 import ChatHeader from '@/components/chat/chat-header';
 import ChatInput from '@/components/chat/chat-input';
+import ChatMessages from '@/components/chat/chat-messages';
 import { currentProfile } from '@/lib/current-profile';
 import { db } from '@/lib/db';
 import { auth } from '@clerk/nextjs/server';
@@ -44,7 +45,20 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
         type='channel'
       />
 
-      <div className='flex-1'>Future Messages</div>
+      <ChatMessages
+        name={channel.name}
+        member={member}
+        chatId={channel.id}
+        apiUrl='/api/messages'
+        socketUrl='/api/socket/messages'
+        socketQuery={{
+          channelId: channel.id,
+          serverId: channel.serverId,
+        }}
+        paramKey='channelId'
+        paramValue={channel.id}
+        type='channel'
+      />
 
       <ChatInput
         name={channel.name}
