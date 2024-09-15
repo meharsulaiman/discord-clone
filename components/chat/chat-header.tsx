1632bc781987898256027e8e3232a48d9ca87930
Sulaiman Shabbir <sulaimanshabbir31@gmail.com>
2024-09-15 09:49:32 +0500

    socket connection

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -2,6 +2,7 @@ import { Hash } from 'lucide-react';
 import React from 'react';
 import MobileToggle from '../mobile-toggle';
 import UserAvatar from '../user-avatar';
+import { SocketIndicator } from '../socket-indicator';
 
 interface ChatHeaderProps {
   serverId: string;
@@ -25,6 +26,10 @@ const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) => {
         <UserAvatar src={imageUrl} className='mr-2 h-6 w-6' />
       )}
       <p className='font-semibold text-md text-black dark:text-white'>{name}</p>
+
+      <div className='ml-auto flex items-center'>
+        <SocketIndicator />
+      </div>
     </div>
   );
 };
--- types.ts
+++ types.ts
@@ -1,5 +1,18 @@
 import { Member, Profile, Server } from '@prisma/client';
 
+import { Socket } from 'net';
+import { Server as SocketIOServer } from 'socket.io';
+import { NextApiResponse } from 'next';
+import { Server as HttpServer } from 'http';
+
+export type NextApiResponseServerIo = NextApiResponse & {
+  socket: Socket & {
+    server: HttpServer & {
+      io: SocketIOServer;
+    };
+  };
+};
+
 export type ServerWithMembersWithProfile = Server & {
   members: (Member & { profile: Profile })[];
 };
