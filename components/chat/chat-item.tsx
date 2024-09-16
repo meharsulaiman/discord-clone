71e75efda705a886ac54500c53e64d942b611cd6
Sulaiman Shabbir <sulaimanshabbir31@gmail.com>
2024-09-16 16:42:49 +0500

    delete message

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -16,6 +16,7 @@ import { Form, FormControl, FormField, FormItem } from '../ui/form';
 import { Input } from '../ui/input';
 import { Button } from '../ui/button';
 import axios from 'axios';
+import { useModal } from '@/hooks/use-modal-store';
 
 interface ChatItemProps {
   id: string;
@@ -55,6 +56,7 @@ const ChatItem = ({
   socketQuery,
 }: ChatItemProps) => {
   const [isEditing, setIsEditing] = useState(false);
+  const { onOpen } = useModal();
   const [isDeleting, setIsDeleting] = useState(false);
 
   useEffect(() => {
@@ -224,7 +226,15 @@ const ChatItem = ({
             </ActionTooltip>
           )}
           <ActionTooltip label='Delete'>
-            <Trash className='cursor-pointer ml-auto w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:hover:text-zinc-300 transition' />
+            <Trash
+              onClick={() =>
+                onOpen('deleteMessage', {
+                  apiUrl: `${socketUrl}/${id}`,
+                  query: socketQuery,
+                })
+              }
+              className='cursor-pointer ml-auto w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:hover:text-zinc-300 transition'
+            />
           </ActionTooltip>
         </div>
       )}
--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -10,13 +10,15 @@ export type ModalType =
   | 'deleteServer'
   | 'deleteChannel'
   | 'editChannel'
-  | 'messageFile';
+  | 'messageFile'
+  | 'deleteMessage';
 
 interface ModalData {
   server?: Server;
   channel?: Channel;
   channelType?: ChannelType;
   apiUrl?: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   query?: Record<string, any>;
 }
 
