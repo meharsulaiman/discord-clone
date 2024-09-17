4ab5201ca1f73257e1ae700652beb53719af8b43
Sulaiman Shabbir <sulaimanshabbir31@gmail.com>
2024-09-17 06:43:54 +0500

    chat realtime

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -125,7 +125,10 @@ const ChatItem = ({
   return (
     <div className='relative group flex items-center hover:bg-black/5 p-4 transition w-full'>
       <div className='group flex gap-x-2 items-start w-full'>
-        <div className='cursor-pointer hover:drop-shadow-md transition'>
+        <div
+          className='cursor-pointer hover:drop-shadow-md transition'
+          onClick={onMemberClick}
+        >
           <UserAvatar src={member.profile.imageUrl} />
         </div>
         <div className='flex flex-col w-full'>
